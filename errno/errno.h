c94995fc77006886e061b55815f2b9929ecdd445
Clyde Bazile <bazileclyde@gmail.com>
2020-03-23 23:13:49 -0400

    add errno source

--- errno/errno.h
+++ errno/errno.h
@@ -1,8 +1,14 @@
 #ifndef _ERRNO
-#define _ERRNO
+#define _errno
 
-#define EDOM 1
-#define ERANGE 2
+#ifndef _YVALS
+#include <yvals.h>
+#endif
+
+#define EDOM _EDOM
+#define ERANGE _ERANGE
+#define EFPOS _EFPOS
+#define _NERR _ERRMAX
 
 extern int errno;
 
